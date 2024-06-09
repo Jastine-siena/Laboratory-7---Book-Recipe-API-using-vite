320bf188bcd1308bc79b60b73eb547956538b384
Jastine <sienamarkjastine34@gmail.com>
2024-06-09 16:04:18 +0800

    ready for deployment

--- src/DataForm.jsx
+++ src/DataForm.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 const DataForm = () => {
-  const API_URL = 'https://spectacular-flan-c1e4b3.netlify.app/.netlify/functions/api/';
+  const API_URL = 'https://cute-puppy-22c254.netlify.app/.netlify/functions/api/';
   const [data, setData] = useState([]);
   const [recipe, setRecipe] = useState('');
   const [ingredients, setIngredients] = useState('');
