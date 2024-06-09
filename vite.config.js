994c19bb59214a625e668fca3c8384f0d4c3be81
Jastine <sienamarkjastine34@gmail.com>
2024-06-09 16:15:57 +0800

    some changes occured

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'Laboratory-7---Book-Recipe-API-using-vite', // Update this line
 })
